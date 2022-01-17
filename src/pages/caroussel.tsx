54c19a7819149d15523e6d32d4a83c9488a817fc
Mehdi-githu <mehdi.jabbour@efrei.net>
2022-01-17 19:01:40 +0100

    'swiperfix'

--- src/pages/caroussel.tsx
+++ src/pages/caroussel.tsx
@@ -29,27 +29,43 @@ export default function Caroussel() {
     <>
 <Layout>
   
-</Layout>
-    {/* <Swiper speed={400}  direction={'horizontal'} slidesPerView={1} spaceBetween={0} mousewheel={{sensitivity:1}}  centeredSlides={true} pagination={{
+
+    <Swiper speed={50}  direction={'vertical'} slidesPerView={1} spaceBetween={0} mousewheel={{sensitivity:1}}  centeredSlides={true} pagination={{
 
   "clickable": true,
 }} breakpoints={{
   "640": {
-    "slidesPerView": 2,
+    "slidesPerView": 1,
     "spaceBetween": 20
   },
   "768": {
-    "slidesPerView": 4,
+    "slidesPerView": 1,
     "spaceBetween": 40
   },
   "1024": {
-    "slidesPerView": 9,
+    "slidesPerView": 1,
     "spaceBetween": 50
   }
 }}
  className="mySwiper">
-<SwiperSlide><img className="filter grayscale hover:grayscale-0"src="https://swiperjs.com/demos/images/nature-1.jpg" /></SwiperSlide><SwiperSlide><img className="filter grayscale hover:grayscale-0"src="https://swiperjs.com/demos/images/nature-2.jpg" /></SwiperSlide><SwiperSlide><img className="filter grayscale hover:grayscale-0"src="https://swiperjs.com/demos/images/nature-3.jpg" /></SwiperSlide><SwiperSlide><img className="filter grayscale hover:grayscale-0"src="https://swiperjs.com/demos/images/nature-4.jpg" /></SwiperSlide><SwiperSlide><img className="filter grayscale hover:grayscale-0"src="https://swiperjs.com/demos/images/nature-5.jpg" /></SwiperSlide><SwiperSlide><img className="filter grayscale hover:grayscale-0"src="https://swiperjs.com/demos/images/nature-6.jpg" /></SwiperSlide><SwiperSlide><img className="filter grayscale hover:grayscale-0"src="https://swiperjs.com/demos/images/nature-7.jpg" /></SwiperSlide><SwiperSlide><img className="filter grayscale hover:grayscale-0"src="https://swiperjs.com/demos/images/nature-8.jpg" /></SwiperSlide><SwiperSlide><img className="filter grayscale hover:grayscale-0"src="https://swiperjs.com/demos/images/nature-9.jpg" /></SwiperSlide>
-  </Swiper> */}
+<SwiperSlide className="flex-col ">          <div className='space-x-60  justify-center items-center '>
+            <span className='n1 text-8xl'>J</span>
+            <span className='n1 text-8xl'>A</span>
+            <span className='n1 text-8xl'>B</span>
+            <span className='n1 text-8xl'>B</span>
+            <span className='n1 text-8xl'>O</span>
+            <span className='n1 text-8xl'>U</span>
+            <span className='n1 text-8xl'>R</span>
+          </div>
+          <div className=' space-x-32 justify-center items-center '>
+            <span className='n1 text-8xl'>M</span>
+            <span className='n1 text-8xl'>E</span>
+            <span className='n1 text-8xl'>H</span>
+            <span className='n1 text-8xl'>D</span>
+            <span className='n1 text-8xl'>I</span>
+
+          </div></SwiperSlide><SwiperSlide><img className="filter grayscale hover:grayscale-0"src="https://swiperjs.com/demos/images/nature-2.jpg" /></SwiperSlide><SwiperSlide><img className="filter grayscale hover:grayscale-0"src="https://swiperjs.com/demos/images/nature-3.jpg" /></SwiperSlide><SwiperSlide><img className="filter grayscale hover:grayscale-0"src="https://swiperjs.com/demos/images/nature-4.jpg" /></SwiperSlide><SwiperSlide><img className="filter grayscale hover:grayscale-0"src="https://swiperjs.com/demos/images/nature-5.jpg" /></SwiperSlide><SwiperSlide><img className="filter grayscale hover:grayscale-0"src="https://swiperjs.com/demos/images/nature-6.jpg" /></SwiperSlide><SwiperSlide><img className="filter grayscale hover:grayscale-0"src="https://swiperjs.com/demos/images/nature-7.jpg" /></SwiperSlide><SwiperSlide><img className="filter grayscale hover:grayscale-0"src="https://swiperjs.com/demos/images/nature-8.jpg" /></SwiperSlide><SwiperSlide><img className="filter grayscale hover:grayscale-0"src="https://swiperjs.com/demos/images/nature-9.jpg" /></SwiperSlide>
+  </Swiper></Layout>
     </>
   )
 }
